0974457bfb46819c33940103b8cb99acef4410cb
Sehoan Choi <swan@Swans-Macbook-Pro.local>
2020-11-02 18:10:37 +0900

    uibuild

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ const personSchema = new mongoose.Schema({
     name: String,
     number: String,
 })
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
 const Person = mongoose.model('Person', personSchema)
 
 app.get('/api/persons', (req, res) => {
